4c48d863168f7ed92b8d757e971f3ae7dac67d1c
Shreyaan Arora <shreyaanarora@engr2-30-34-dhcp.int.colorado.edu>
2022-11-11 19:21:09 -0700

    Inital Files Commit

---
+++ LetsTalk/LetsTalk/LetsTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsTalkApp.swift
+//  LetsTalk
+//
+//  Created by Shreyaan Arora on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
